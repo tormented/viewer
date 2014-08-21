b2be1079a908789ae267868baf023abeb41c9f9a
Vitaliy Melnik <root@Mac-mini-Slava.local>
2014-08-21 10:49:37 +0300

    update path to presentation

--- www/PresentationViewer.js
+++ www/PresentationViewer.js
@@ -3,13 +3,13 @@ var PresentationViewer = {
 	ENTRY_FILENAME: 'index.html',
 	EVENT_DID_LOAD: 'DID_LOAD',
 	EVENT_ON_COMPLETE: 'ON_COMPLETE',
-	openPresentation: function(id){
+	openPresentation: function(fullPath){
 		var presentationIndexPath;
-		presentationIndexPath = this._pathWithPathComponent(id, this.ENTRY_FILENAME);
+		presentationIndexPath = this._pathWithPathComponent(fullPath, this.ENTRY_FILENAME);
 		return cordova.exec(this._presentationViewingHandler.bind(this), this._presentationViewingErrorHandler, this.PLUGIN_NAME, 'openPresentation', [presentationIndexPath]);
 	},
-	_pathWithPathComponent: function(id, pathComponent){
-		return cordova.file.applicationStorageDirectory + "Documents/presentations/" + id + "/" + pathComponent;
+	_pathWithPathComponent: function(path, pathComponent){
+		return  path + "/" + pathComponent;
 	},
 	_presentationViewingHandler: function(message){
 		var that = this;
